68dc914cc974837e64a17c8f58cf97579b184201
yefei <8196>
2018-05-08 17:44:35 +0800

    修改investmentfragment

--- app/src/main/java/com/itheima/testnewproject/module/investment/InvestmentFragment.java
+++ app/src/main/java/com/itheima/testnewproject/module/investment/InvestmentFragment.java
@@ -1,13 +1,21 @@
 package com.itheima.testnewproject.module.investment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.blankj.utilcode.utils.ToastUtils;
 import com.itheima.testnewproject.R;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,16 +30,101 @@ import butterknife.ButterKnife;
  * 更新描述   ${TODO}
  */
 
-public class InvestmentFragment extends Fragment {
-    @Bind(R.id.tv)
-    TextView tv;
+public class InvestmentFragment extends Fragment implements RecyclerViewLeftAdapter.OnItemClickListener, RecyclerViewRightAdapter.OnRightItemClickListener {
+    @Bind(R.id.classify_left_list)
+    RecyclerView mRecyclerView;
+    @Bind(R.id.classify_right_list)
+    RecyclerView mRecyclerViewRight;
+
+    private List<String> dataTitle = new ArrayList<>();
+    private RecyclerViewLeftAdapter indexQueryLeftAdapter;
+
+    private List<String> data = new ArrayList<>();
+    private RecyclerViewRightAdapter indexQueryRightAdapter;
+    private String taskPasition;
+    private LinearLayoutManager mLinearLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = View.inflate(getActivity(),R.layout.fragment_home,null);
+        View view = View.inflate(getActivity(), R.layout.fragment_home, null);
         //        View view = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
-//        ToastUtils.showShortToast("这是怎么了");
+        //        ToastUtils.showShortToast("这是怎么了");
         return view;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        for (int i = 0; i < 20; i++) {
+            dataTitle.add("Android:\t" + i);
+        }
+        indexQueryLeftAdapter = new RecyclerViewLeftAdapter(getActivity(), dataTitle);
+        mRecyclerView.setAdapter(indexQueryLeftAdapter);
+        indexQueryLeftAdapter.setOnRecyclerItemClickListener(this);
+
+        mRecyclerViewRight.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        for (int i = 0; i < 11; i++) {
+            data.add("" + i);
+        }
+        indexQueryRightAdapter = new RecyclerViewRightAdapter(getActivity(), data);
+        mRecyclerViewRight.setAdapter(indexQueryRightAdapter);
+        indexQueryRightAdapter.setOnRightItemClickListener(this);
+        mRecyclerView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        mRecyclerViewRight.setOverScrollMode(View.OVER_SCROLL_NEVER);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+    @Override
+    public void onLeftItemClick(int position) {
+        taskPasition = dataTitle.get(position);
+        List<String> lineType = new ArrayList<>();
+        for (int i = 0; i < Math.random() * 10; i++) {
+            lineType.add(Integer.parseInt(new DecimalFormat("0").format(Math.random() * 10)) + "");
+        }
+        indexQueryRightAdapter.updateData(lineType);
+//        scrollToTop(position);
+    }
+
+    @Override
+    public void onRightItemClick(int position) {
+
+        //        showToast("You clicked:\t" + position);
+        ToastUtils.showShortToast( "You clicked:\t" + position);
+    }
+
+   /* private void scrollToTop(int n) {
+        //先从RecyclerView的LayoutManager中获取第一项和最后一项的Position
+        int firstItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+        int lastItem = mLinearLayoutManager.findLastVisibleItemPosition();
+        printLogd("firstItem:\t" + firstItem);
+        printLogd("lastItem:\t" + lastItem);
+        //然后区分情况
+        if (n <= firstItem) {
+            //当要置顶的项在当前显示的第一个项的前面时
+            //            rvRecyclerView.scrollToPosition(n);//有bug
+            mRecyclerView.smoothScrollBy(0, mRecyclerView.getChildAt(n - firstItem).getTop(), new LinearInterpolator());
+        } else if (n <= lastItem) {
+            //当要置顶的项已经在屏幕上显示时
+            int top = mRecyclerView.getChildAt(n - firstItem).getTop();
+            mRecyclerView.smoothScrollBy(0, top, new LinearInterpolator());
+            //            mRecyclerView.smoothScrollToPosition(n);
+        } else {
+            //当要置顶的项在当前显示的最后一项的后面时
+            mRecyclerView.scrollToPosition(n);
+
+            //这里这个变量是用在RecyclerView滚动监听里面的
+            //            move = true;
+        }
+}*/
 }
---
+++ app/src/main/java/com/itheima/testnewproject/module/investment/RecyclerViewRightAdapter.java
@@ -0,0 +1,139 @@
+package com.itheima.testnewproject.module.investment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.itheima.testnewproject.R;
+
+import java.util.List;
+
+/**
+ * Created by <a href="http://blog.csdn.net/student9128">Kevin</a> on 2017/7/26.
+ * <p>
+ * <h3>Description:</h3>
+ * <p/>
+ * <p/>
+ */
+
+
+public class RecyclerViewRightAdapter extends RecyclerView.Adapter<RecyclerViewRightAdapter.MyViewHolder> {
+    private Context context;
+    private List<String> data;
+    private int[] icon;
+
+    public RecyclerViewRightAdapter(List<String> data) {
+        this.data = data;
+    }
+
+    public RecyclerViewRightAdapter(Context context, List<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    public RecyclerViewRightAdapter(Context context, List<String> data, int[] icon) {
+        this.context = context;
+        this.data = data;
+        this.icon = icon;
+    }
+
+    public void addItem() {
+//        data.add("xxx");
+        notifyItemInserted(0);
+    }
+
+    public void updateData(List<String> d) {
+        data.clear();
+        data.addAll(d);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_item_recycler_right, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        String img = data.get(position);
+        if ("0".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_oreo_1);
+            holder.textView.setText("Android O");
+        } else if ("1".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_n_1);
+            holder.textView.setText("Android N");
+        } else if ("2".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_m_1);
+            holder.textView.setText("Android M");
+        } else if ("3".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_lollipop_1);
+            holder.textView.setText("Android L");
+        } else if ("4".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_oreo_2);
+            holder.textView.setText("Android O");
+        } else if ("5".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_oreo_3);
+            holder.textView.setText("Android O");
+        } else if ("6".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_n_2);
+            holder.textView.setText("Android N");
+        } else if ("7".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_lollipop_2);
+            holder.textView.setText("Android L");
+        } else if ("8".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_oreo_4);
+            holder.textView.setText("Android O");
+        } else if ("9".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_m_2);
+            holder.textView.setText("Android M");
+        } else if ("10".equals(img)) {
+            holder.imageView.setImageResource(R.drawable.ic_icon);
+            holder.textView.setText("Android");
+        }
+
+        holder.llItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onRightItemClick(position);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textView;
+        LinearLayout llItem;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.iv_image);
+            textView = (TextView) itemView.findViewById(R.id.tv_text);
+            llItem = (LinearLayout) itemView.findViewById(R.id.ll_item);
+        }
+    }
+
+    public void setOnRightItemClickListener(OnRightItemClickListener l) {
+        listener = l;
+    }
+
+    private OnRightItemClickListener listener;
+
+    public interface OnRightItemClickListener {
+        void onRightItemClick(int position);
+    }
+}
